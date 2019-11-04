4ae337f2baa4ccbfa3e0c5ce4b87ee7042806217
zakia <zakia@Haier-PC>
2019-11-04 11:17:54 +0500

    Decorator Pattern

---
+++ DecoratorPattern/src/decoratorpattern/ShapeDecorator.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package decoratorpattern;
+
+/**
+ *
+ * @author hp
+ */
+public abstract class ShapeDecorator implements Shape {
+   protected Shape decoratedShape;
+
+   public ShapeDecorator(Shape decoratedShape){
+      this.decoratedShape = decoratedShape;
+   }
+
+   @Override
+   public void draw(){
+      decoratedShape.draw();
+   }	
+}
