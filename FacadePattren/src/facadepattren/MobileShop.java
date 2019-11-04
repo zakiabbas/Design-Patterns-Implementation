477a10112efa1c28265d47419137ac7e37a1c7ac
zakia <zakia@Haier-PC>
2019-11-04 11:06:00 +0500

    Facade Pattern

---
+++ FacadePattren/src/facadepattren/MobileShop.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facadepattren;
+
+/**
+ *
+ * @author sp16-bse-072
+ */
+public class MobileShop {
+    
+}
